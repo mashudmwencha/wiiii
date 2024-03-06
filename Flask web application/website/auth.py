5a4cd650ec572c3c0b1d55a0737bb5c889366b68
Quencha <91719095+mashudmwencha@users.noreply.github.com>
2024-03-06 16:50:06 +0300

    Add files via upload

--- Flask web application/website/auth.py
+++ Flask web application/website/auth.py
@@ -12,5 +12,24 @@ def logout ():
     return "<p>Logout</p>"
 
 @auth.route('/sign-up', methods=['GET', 'POST'])
-def sign_out ():
+def sign_up ():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        firstName = request.form.get('firstName')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+        
+        if len(email) < 4:
+            pass
+        elif len(firstName) < 2:    
+            pass
+        elif password1 != password2:    
+            pass    
+        elif len(password1) < 7:    
+            pass
+        else:
+            # add user to the database
+            pass
+
+        
     return render_template("sign_up.html")
