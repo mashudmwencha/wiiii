336bcad03df90f2fb2ff431ed64b33891bd23cdc
Quencha <91719095+mashudmwencha@users.noreply.github.com>
2024-03-07 16:57:02 +0300

    Add files via upload

--- Flask web application/website/auth.py
+++ Flask web application/website/auth.py
@@ -1,5 +1,12 @@
 
-from flask import Blueprint, render_template, request 
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+from .models import User  
+import hashlib
+# password hashing
+from werkzeug.security import generate_password_hash, check_password_hash
+from . import db
+
+
 
 auth = Blueprint('auth', __name__)
 
@@ -20,16 +27,22 @@ def sign_up ():
         password2 = request.form.get('password2')
         
         if len(email) < 4:
-            pass
+            flash('Email must be greater than 3 characters', category='error')
         elif len(firstName) < 2:    
-            pass
+            flash ('First name must be greater than 1 character', category='error')
         elif password1 != password2:    
-            pass    
+            flash ('Passwords don\'t match', category='error')    
         elif len(password1) < 7:    
-            pass
+            flash ('Password must be at least 7 characters', category='error')
         else:
-            # add user to the database
-            pass
+            # add user to database
+            new_user = User(email=email, firstName=firstName, password=generate_password_hash(password1, method='sha256'))
+            db.session.add(new_user)
+            db.session.commit()
+            flash ('Account created', category='success')
+            return redirect(url_for('views.home'))
+            
+            
 
         
     return render_template("sign_up.html")
